2968f48db6c4574ed392703e856ba0095eb32e16
Corbin Muraro <corbinmuraro@gmail.com>
2016-12-20 03:18:45 -0800

    Add full photo + video modal in header; begin kickstarter info at bottom

--- js/main.js
+++ js/main.js
@@ -1,5 +1,44 @@
-$(document).ready(function () { 
-	$('.button').click(function(){
-        $(this).toggleClass("hover");
+$(document).ready(function(){
+    kickstarter();
+    venobox();
+});
+
+function venobox() {
+    /* default settings */
+    $('.venobox').venobox(); 
+
+
+    var w = Math.floor( $( window ).width() / 1.5);
+    var h = Math.floor( $( window ).height() / 1.5);
+
+    w = JSON.stringify(w);
+    h = JSON.stringify(h);
+
+    /* custom settings */
+    $('.venobox_custom').venobox({
+        framewidth: w,              // default: ''
+        frameheight: h,             // default: ''
+        bgcolor: '#000',         // default: '#fff'
+        titleattr: 'data-title',    // default: 'title'
+        numeratio: true,            // default: false
+        infinigall: true            // default: false
     });
-	});
+}
+
+function kickstarter() {
+    $.ajax({
+    url: 'https://www.kickstarter.com/projects/portefino/portefini-the-everyday-blazer-for-all-occasions-an',
+    type: 'GET',
+    success: function(res) {
+        var data = $.parseHTML(res);  //<----try with $.parseHTML().
+        $(data).find('div.js-pledged').each(function(){
+            $('.amount-pledged').append($(this).html());
+        });
+
+        $(data).find('div.')
+
+        $('body').toggleClass('loaded');
+    }
+    });
+}
+
