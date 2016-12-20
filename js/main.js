d508f1837bdb652966a6e62db48c554e818cc345
Corbin Muraro <corbinmuraro@gmail.com>
2016-12-19 21:22:18 -0800

    Add preload, polish content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-$(document).ready(function () { 
-	$('.button').click(function(){
-        $(this).toggleClass("hover");
-    });
-	});
+// $(document).ready(function () { 
+// 	$('.button').click(function(){
+//         $(this).toggleClass("hover");
+//     });
+// 	});
+
+
+$.ajax({
+	url: 'https://www.kickstarter.com/projects/portefino/portefini-the-everyday-blazer-for-all-occasions-an',
+	type: 'GET',
+	success: function(res) {
+		var data = $.parseHTML(res);  //<----try with $.parseHTML().
+		$(data).find('div.js-pledged').each(function(){
+			$('.total-pledged').append($(this).html());
+		});
+
+		$('body').toggleClass('loaded');
+	}
+ });
