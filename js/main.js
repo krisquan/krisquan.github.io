ae22d663dacf960cb4b9751966d23d9a8a62443a
Corbin Muraro <corbinmuraro@gmail.com>
2016-12-20 16:19:14 -0800

    Progress on kickstarter section

--- js/main.js
+++ js/main.js
@@ -26,19 +26,54 @@ function venobox() {
 }
 
 function kickstarter() {
-    $.ajax({
-    url: 'https://www.kickstarter.com/projects/portefino/portefini-the-everyday-blazer-for-all-occasions-an',
-    type: 'GET',
-    success: function(res) {
-        var data = $.parseHTML(res);  //<----try with $.parseHTML().
-        $(data).find('div.js-pledged').each(function(){
-            $('.amount-pledged').append($(this).html());
-        });
+    var amountPledged = 500;
+    var goal = 9000;
+    var backers = 67;
 
-        $(data).find('div.')
+    var percentPledgedInt = amountPledged / goal * 100;
+    percentPledgedInt = Math.floor(percentPledgedInt);
+    var percentPledgedString = percentPledgedInt.toString();
+    console.log(percentPledgedString);
 
-        $('body').toggleClass('loaded');
-    }
-    });
+    $('.progress-meter').animate({
+        width: percentPledgedString + "%"
+    }, 1500);
+
+    $('.amount-pledged').animateNumber({
+        number: amountPledged
+    }, 1500);
+
+    $('.percent-funded').animateNumber({
+        number: percentPledgedInt
+    }, 1500);
+
+    $('.num-backers').animateNumber({
+        number: backers
+    }, 1500);
 }
+
+
+// // utility function to see if element is in viewport
+// function isScrolledIntoView(elem) {
+//     var docViewTop = $(window).scrollTop();
+//     var docViewBottom = docViewTop + $(window).height();
+
+//     var elemTop = $(elem).offset().top;
+//     var elemBottom = elemTop + $(elem).height();
+
+//     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// }
+
+
+    // $.ajax({
+    // url: 'https://www.kickstarter.com/projects/portefino/portefini-the-everyday-blazer-for-all-occasions-an',
+    // type: 'GET',
+    // success: function(res) {
+    //     var data = $.parseHTML(res);  //<----try with $.parseHTML().
+    //     $(data).find('div.js-pledged').each(function(){
+    //         $('.amount-pledged').append($(this).html());
+    //     });
+    // }
+    // });
+
 
